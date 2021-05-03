87fc230cbdc05d3fb84e01591849a3e1689ff77a
Petrovich1973 <designtorg@ya.ru>
2021-05-03 20:15:51 +0300

    added cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const config = require('config')
 const path = require('path')
 const mongoose = require('mongoose')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json({ extended: true }))
 
 app.use('/api/auth', require('./routes/auth.routes'))
